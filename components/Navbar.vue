85aee5254aab12dd111d3b3454e7311a6c89ca4f
prostiwlsn <prostiwlsn@gmail.com>
2024-03-26 22:45:06 +0700

    Update navbar styles

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="top-line">
         <div class="navbar">
-            <strong><h3 style="margin: 0%; color: #EEE4B1">ToDoList</h3></strong>
+            <strong><h2 style="margin: 0%; color: #EEE4B1">ToDoList</h2></strong>
             <button style="margin: 3px;">New list</button>
         </div>
     </div>
@@ -33,19 +33,20 @@
 
 button {
   border-radius: 8px;
-  border: 1px solid transparent;
+  border: 2px solid;
+  border-color: #8C6A5D;
   padding: 0.6em 1.2em;
   font-size: 1em;
   font-weight: 500;
   font-family: inherit;
-  background-color: #430A5D;
+  background-color: hsl(281, 81%, 20%, 0);
   color: #EEE4B1;
   cursor: pointer;
   transition: border-color 0.25s;
-  border: none;
+  text-shadow: 0px 2px #8C6A5D;
 }
 button:hover {
-  background-color: #5F374B;
+  background-color: #8C6A5D;
 }
 button:focus,
 button:focus-visible {
