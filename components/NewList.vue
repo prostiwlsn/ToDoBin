1b3fb28c02a42d22206d15fe8f025a94a00dcc45
prostiwlsn <prostiwlsn@gmail.com>
2024-03-29 23:39:46 +0700

    Add test element

--- components/NewList.vue
+++ components/NewList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="list-item">
-        <div>page content</div>
-        <div>page content</div>
+        <h3 style="margin: 0%; padding:0%; margin-right: 3px">1.</h3>
+        <textarea placeholder="Your plans here"></textarea>
     </div>
 </template>
 
@@ -17,6 +17,26 @@ onMounted(() => {
 .list-item{
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: start;
+}
+
+input{
+    background: none;
+    border: none;
+    width: 100%;
+}
+
+textarea{
+    background: none;
+    border: none;
+    width: 100%;
+    height: 18px;
+    resize: none;
+    font-size: 18px;
+}
+
+textarea:focus{
+    border: none;
+    outline: none;
 }
 </style>
