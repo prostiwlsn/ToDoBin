458bd3fc64fa7f83f5914bb28cc20ff3b85a34ba
prostiwlsn <prostiwlsn@gmail.com>
2024-03-30 14:55:49 +0700

    Add auto-resize

--- components/NewList.vue
+++ components/NewList.vue
@@ -1,42 +1,14 @@
 <template>
-    <div class="list-item">
-        <h3 style="margin: 0%; padding:0%; margin-right: 3px">1.</h3>
-        <textarea placeholder="Your plans here"></textarea>
-    </div>
+
+    <NewListElement/>
+    
 </template>
 
 <script setup>
-const list = ref([])
+const list = ref([""])
 
-onMounted(() => {
-    //console.log($refs.navbar.clientHeight)
-})
 </script>
 
 <style scoped>
-.list-item{
-    display: flex;
-    flex-direction: row;
-    justify-content: start;
-}
 
-input{
-    background: none;
-    border: none;
-    width: 100%;
-}
-
-textarea{
-    background: none;
-    border: none;
-    width: 100%;
-    height: 18px;
-    resize: none;
-    font-size: 18px;
-}
-
-textarea:focus{
-    border: none;
-    outline: none;
-}
 </style>
---
+++ components/NewListElement.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="list-item">
+        <h3 style="margin: 0%; padding:0%; margin-right: 3px">1.</h3>
+        <textarea placeholder="Your plans here" v-model="listElement" :style="{height: elementHeight + 'px'}" ref="textarea"></textarea>
+    </div>
+</template>
+
+<script setup>
+//const textarea = document.querySelector('textarea');
+
+const listElement = ref("")
+const textarea = ref(null)
+
+const elementId = 1
+
+const baseHeight = 18
+const elementHeight = ref(18)
+
+watch(listElement, (newElem, oldElem) => {
+    //console.log(JSON.stringify(listElement.value))
+
+    //let element = this.$refs["textarea"];
+    //element.style.height = element.scrollHeight + "px";
+
+    elementHeight.value = textarea.value.scrollHeight
+
+    console.log(textarea.value.scrollHeight,textarea.value.style.height)
+})
+
+onMounted(() => {
+    //textarea.value.focus()
+})
+</script>
+
+<style scoped>
+.list-item{
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+}
+
+input{
+    background: none;
+    border: none;
+    width: 100%;
+}
+
+textarea{
+    background: none;
+    border: none;
+    width: 100%;
+    resize: none;
+    font-size: 18px;
+}
+
+textarea:focus{
+    border: none;
+    outline: none;
+}
+</style>
