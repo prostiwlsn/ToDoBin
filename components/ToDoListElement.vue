ca3aa4fe9e210af04ddac3ba60e6bba8995a7525
prostiwlsn <prostiwlsn@gmail.com>
2024-04-03 01:04:33 +0700

    Add new server route

---
+++ components/ToDoListElement.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps(['listId', 'lineId'])
+const emit = defineEmits(['check', 'uncheck'])
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
---
+++ pages/list/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue';
+
+const route = useRoute()
+const content = await $fetch('/api/list/' + route.params.id, {
+        method: 'get',
+    })
+
+onMounted(() => {
+    console.log(route.params.id)
+    console.log(content)
+})
+</script>
+
+<template>
+    <div style="margin-top: 60px" class="content">{{route.params.id}}</div>
+</template>
